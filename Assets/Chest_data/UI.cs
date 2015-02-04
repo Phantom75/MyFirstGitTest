7a4fee7a02c3b301d7e9694690c201eb8cbed379
Phantom75 <martin@schacher.ch>
2015-02-04 22:52:50 +0100

    Initial Files

---
+++ Assets/Chest_data/UI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Open Chest"))
+        {
+            transform.animation.Play("Chest_Open");
+        }
+
+        if (GUILayout.Button("Close Chest"))
+        {
+            transform.animation.Play("Chest_Close");
+        }
+    }
+}
